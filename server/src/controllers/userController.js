b4d92f4ee0511eb54137febb18c9b779af26b61a
dianasim <diana.simeonova56@gmail.com>
2025-06-23 21:12:36 +0300

    add password hashing

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import userService from "../services/userService.js";
 
 const userController = Router();
 
-userController.post('/register', (req, res) => {
-    console.log(req.body);
+userController.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    const result = await userService.register(userData);
 
     res.end();
 })
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import {Schema, model} from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
     email: {
@@ -11,6 +12,9 @@ const userSchema = new Schema({
     }
 });
 
+userSchema.pre('save', async function() {
+    this.password = await bcrypt.hash(this.password, 10);
+})
 
 const User = model('User', userSchema);
 
