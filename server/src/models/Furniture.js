78d60aeaff3104a8a194bc83268c053400262215
dianasim <diana.simeonova56@gmail.com>
2025-06-24 11:29:17 +0300

    add furniture validation

--- server/src/models/Furniture.js
+++ server/src/models/Furniture.js
@@ -4,26 +4,33 @@ const furnitureSchema = new Schema({
     make: {
         type: String,
         required: true,
+        minLength: [4, "Make should be at least 4 chars long"]
     },
     model: {
         type: String,
         required: true,
+        minLength: [4, "Model should be at least 4 chars long"]
     },
     year: {
         type: Number,
         required: true,
+        min: [1950, 'Year cant be lower than 1950'],
+        max: [2050, 'year cannot be higher than 2050']
     },
     description: {
         type: String,
         required: true,
+        minLength: [10, 'description should be at least 10 chars long']
     },
     price: {
         type: Number,
         required: true,
+        min: [0, 'price shoudl be a positive num']
     },
     img: {
         type: String,
-        required: true,
+        required: [true, 'img field is required'],
+        validate: [/^https?:\/\//, 'invalid image url']
     },
     material: {
         type: String
