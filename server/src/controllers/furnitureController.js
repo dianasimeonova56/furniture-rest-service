8dc95016ecdb87c02e38ea317f3bb4abf3dde818
dianasim <diana.simeonova56@gmail.com>
2025-06-24 11:19:35 +0300

    save furniture to db

--- server/src/controllers/furnitureController.js
+++ server/src/controllers/furnitureController.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
+import furnitureService from '../services/furnitureService.js';
 
 const furnitureController = Router();
 
 furnitureController.post('/', async (req, res) => {
     const furnitureData = req.body;
 
-    res.end();
+    const result = await furnitureService.create(furnitureData)
+
+    res.json(result);
 })
 
 export default furnitureController
